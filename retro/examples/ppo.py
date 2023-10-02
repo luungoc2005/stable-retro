d0d4602266af4f3ee5eb36fdb14928fa95fb8048
Ngoc Nguyen <luungoc2005@gmail.com>
2023-10-03 01:56:33 +0800

    fixed compiling on M1 Mac plus stuffs

--- retro/examples/ppo.py
+++ retro/examples/ppo.py
@@ -85,17 +85,39 @@ def main():
     parser.add_argument("--scenario", default=None)
     args = parser.parse_args()
 
-    def make_env():
-        env = make_retro(game=args.game, state=args.state, scenario=args.scenario)
+    def make_env(render_mode="human"):
+        env = make_retro(game=args.game, state=args.state, scenario=args.scenario, render_mode=render_mode)
         env = wrap_deepmind_retro(env)
         return env
 
-    venv = VecTransposeImage(VecFrameStack(SubprocVecEnv([make_env] * 8), n_stack=4))
+    venv = VecTransposeImage(VecFrameStack(SubprocVecEnv([make_env] * 8, start_method="spawn"), n_stack=4))
+    tb_log_name = f"ppo-{args.game}"
+
+    def on_finish(model):
+        model.save(tb_log_name)
+
+        # save images
+        import imageio
+        images = []
+
+        new_venv = VecTransposeImage(VecFrameStack(SubprocVecEnv([lambda: make_env(render_mode="rgb_array")] * 9, start_method="spawn"), n_stack=4))
+
+        obs = new_venv.reset()
+        img = new_venv.render(mode="rgb_array")
+        total_frames = 1000
+        for _ in range(total_frames):
+            images.append(img)
+            action, _ = model.predict(obs, deterministic=True)
+            obs, _, _ ,_ = new_venv.step(action)
+            img = new_venv.render(mode="rgb_array")
+
+        imageio.mimsave(f"gifs/{tb_log_name}.gif", [np.array(img) for i, img in enumerate(images) if i%2 == 0], duration=total_frames // 29)
+
     model = PPO(
         policy="CnnPolicy",
         env=venv,
         learning_rate=lambda f: f * 2.5e-4,
-        n_steps=128,
+        n_steps=256,
         batch_size=32,
         n_epochs=4,
         gamma=0.99,
@@ -103,11 +125,21 @@ def main():
         clip_range=0.1,
         ent_coef=0.01,
         verbose=1,
+        tensorboard_log="tb_logs",
+        device="mps"
     )
-    model.learn(
-        total_timesteps=100_000_000,
-        log_interval=1,
-    )
+    try:
+        model.learn(
+            total_timesteps=10_000_000,
+            log_interval=1,
+            progress_bar=True,
+            tb_log_name=tb_log_name
+        )
+    except KeyboardInterrupt:
+        on_finish(model)
+        raise
+
+    on_finish(model)
 
 
 if __name__ == "__main__":
